9aaa2140898b7649261b404ef56d9176640fe3e1
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-11-06 13:07:09 -0400

    feat: add paint background utility function to base view

--- data/views/base.lua
+++ data/views/base.lua
@@ -1,3 +1,4 @@
+local tt = require 'tt'
 local Object = require 'object'
 
 local BaseView = Object:extend()
@@ -20,6 +21,17 @@ function BaseView:onTextInput(char, mod)
 end
 
 function BaseView:draw()
+end
+
+function BaseView:update()
+end
+
+function BaseView:paintBackground(color)
+	tt.drawRect {
+		x1 = self.pos.x, x2 = self.size.w,
+		y1 = self.pos.y, y2 = self.size.h,
+		color = color
+	}
 end
 
 return BaseView
