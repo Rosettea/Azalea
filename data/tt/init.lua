64f9f9d8c3f9ca54cce2a73e658080def8ca54ff
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-11-12 10:38:05 -0400

    fix(tt): draw rect with expected width and height

--- data/tt/init.lua
+++ data/tt/init.lua
@@ -2,8 +2,8 @@ local lowkey = require 'lowkey'
 local tt = {}
 
 function tt.drawRect(opts)
-	for row = opts.y1, opts.y2 do
-		for col = opts.x1, opts.x2 do
+	for row = opts.y, opts.y + opts.h do
+		for col = opts.x, opts.x + opts.w do
 			lowkey.paint(col, row, ' ', {
 				fg = opts.color,
 				bg = opts.color
