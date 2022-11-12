2d47ea72a3f4edab448ef9ecbcfb833f4e5f2fd3
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-11-12 13:37:42 -0400

    feat(tt): add write function to draw text

--- data/tt/init.lua
+++ data/tt/init.lua
@@ -4,12 +4,18 @@ local tt = {}
 function tt.drawRect(opts)
 	for row = opts.y, opts.y + opts.h do
 		for col = opts.x, opts.x + opts.w do
-			lowkey.paint(col, row, ' ', {
-				fg = opts.color,
-				bg = opts.color
-			})
+			lowkey.paint(col, row, ' ', opts.color)
 		end
 	end
 end
 
+function tt.write(x, y, text, color)
+	for i = 1, #text do
+		local c = text:sub(i, i)
+		lowkey.paint(x + i - 1, y, c, {
+			fg = color.fg,
+			bg = color.bg 
+		})
+	end
+end
 return tt
