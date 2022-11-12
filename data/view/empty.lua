a1635ed5114b8df7c0c7d38ecd93c8c80dc155ec
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2022-11-12 12:34:24 -0400

    refactor: remove input handler from empty view

--- data/view/empty.lua
+++ data/view/empty.lua
@@ -2,13 +2,7 @@ local View = require 'view'
 
 local Empty = View:extend()
 
-function Empty:onTextInput()
-	self.no = true
-	self:paintBackground {r = 255, b = 0, g = 0}
-end
-
 function Empty:draw()
-	if self.no then return end
 	self:paintBackground {r = 0, b = 0, g = 255}
 end
 
